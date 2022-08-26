eb753e6ebd86b6761beb343b0e1942c12935e119
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-08-25 21:58:46 -0400

    commit tree 014f9b9f288cadd3e35bd59685ca890cc8a49e1d

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+// function superbowlWin(array) {
+//     // declare a variable 'result' assigned to the find() method
+//     let result = array.find(element => element.result === 'W')
+//     // find method has an argument of anon function which 'looks' through result obj for 'W'
+//     if (result) {
+//     // if result is found return the the year result, dot notation on year
+//     return result.year
+//    }
+// }
+
+function superbowlWin(array) {
+   const result = array.find(rec => rec.result === 'W')
+   console.log(result)
+   if (result) {
+    return result.year
+   }
+}
